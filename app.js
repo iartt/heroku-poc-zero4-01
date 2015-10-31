7e486f4be1f1206f7b541d7e000f69f69f975121
iartt <iartt.email@gmail.com>
2015-10-31 23:31:36 +0800

    fixed index not found

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use(['/', '/index'], routes);
 app.use('/*sidebar', routes);
 app.use('/users', users);
 
